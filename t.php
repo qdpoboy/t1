2853d084942f24f988dfa312afa5189139ef59e3
qdpoboy <280597526@qq.com>
2021-09-26 17:06:57 +0800

    测试

--- t.php
+++ t.php
@@ -8,3 +8,4 @@ var_dump(microtime());
 //var_dump(file_get_contents('https://www.baidu.com'));
 var_dump(date('Y-m-d');
 var_dump(array_values([1 => 'hello', 2 => 'world']));
+var_dump(array_keys([1 => 'hello', 2 => 'world']));
